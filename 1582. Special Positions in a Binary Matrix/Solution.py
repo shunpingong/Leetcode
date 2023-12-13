e1ed3db0f38c33da7bef144234d235f8bffb1e04
Ong Shun Ping <shunping23@gmail.com>
2023-12-13 11:28:50 +0800

    Update Solution.py

--- 1582. Special Positions in a Binary Matrix/Solution.py
+++ 1582. Special Positions in a Binary Matrix/Solution.py
@@ -4,22 +4,19 @@ class Solution(object):
         :type mat: List[List[int]]
         :rtype: int
         """
-        m = len(mat)
-        n = len(mat[0])
-        row_count = [0] * m
-        col_count = [0] * n
+        row_count = [0] * len(mat)
+        col_count = [0] * len(mat[0])
         
-        for row in range(m):
-            for col in range(n):
+        for row in range(len(mat)):
+            for col in range(len(mat[0])):
                 if mat[row][col] == 1:
                     row_count[row] += 1
                     col_count[col] += 1
         
         ans = 0
-        for row in range(m):
-            for col in range(n):
+        for row in range(len(mat)):
+            for col in range(len(mat[0])):
                 if mat[row][col] == 1:
                     if row_count[row] == 1 and col_count[col] == 1:
                         ans += 1
-
         return ans
